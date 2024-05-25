1b700817e4d9e6c150ab51fa3f011a215aed8d52
Iroegbu David <iroegbu.dgit@gmail.com>
2024-05-25 19:59:43 +0100

    new commit

--- src/index.ts
+++ src/index.ts
@@ -40,31 +40,34 @@ app.use(bodyParser.json());
 
 // handling web push
 
-if (!public_vipid_key || !private_vipid_key){
-    throw new Error ('Private and Public Vipid keys not found')
+if (!public_vipid_key || !private_vipid_key) {
+    throw new Error('Private and Public Vapid keys not found');
 }
 
 webpush.setVapidDetails('mailto:ireugbudavid@gmail.com', public_vipid_key, private_vipid_key);
 
 app.post('/subscribe', (req, res) => {
-    const {subscription, url} = req.body;
+    const { subscription, url } = req.body;
+    console.log('Received subscription:', subscription);
 
-    console.log('subscription : ',subscription)
-
-    
     const payloadData = {
-        title: 'Push Notification Title',
-        body: 'Notification body entered by David',
-        icon: 'https://images.pexels.com/photos/5083013/pexels-photo-5083013.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-        url: url
+    title: 'Push Notification Title',
+    body: 'Notification body entered by David',
+    icon: 'https://images.pexels.com/photos/5083013/pexels-photo-5083013.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    url: url,
     };
-    res.status(201).json({data: payloadData});
 
     const payload = JSON.stringify(payloadData);
 
     webpush.sendNotification(subscription, payload)
-    .then(()=> console.log('Push notification sent successfully'.blue.bold))
-    .catch(err => console.error(err));
+    .then(() => {
+        console.log('Push notification sent successfully');
+        res.status(201).json({ data: payloadData });
+    })
+    .catch(err => {
+        console.error('Error sending notification:', err);
+        res.status(500).json({ error: 'Error sending notification' });
+    });
 });
 
 // const serverKey = 'BAoq8SpI6kX6dxoWvYtLAkrzOirNOK6vaWI93D-4y0A8zbRe6LEWgb208TnmEa-vK1N6gSKpiFP9JODKIY4ueD8';
