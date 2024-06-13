6c83432e59bf1d697b6e4d2f571f9647dae5fe3b
Iroegbu David <iroegbu.dgit@gmail.com>
2024-06-13 18:13:59 +0100

    new commit

--- src/validations/authValidation.ts
+++ src/validations/authValidation.ts
@@ -132,8 +132,8 @@ export const videoCallValidation = async ( data:any) => {
         const schema = Joi.object({
             meeting_id: Joi.string().trim().required(),
             receiver_id: Joi.string().trim().required(),
-            caller_id: Joi.string().trim().required,
-            token: Joi.string().trim().required,
+            caller_id: Joi.string().trim().required(),
+            token: Joi.string().trim().required(),
         })
         
         
